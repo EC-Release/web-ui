7fcca743b43fa947cc115db1334ce9c6b3d93212
安田　アポロ <ayasuda2003@gmail.com>
2020-02-03 22:55:13 -0800

    code clean up

--- webui-assets/godoc/lib/godoc/godocs.js
+++ webui-assets/godoc/lib/godoc/godocs.js
@@ -1,7 +1,3 @@
-{"trustedIssuerIds": ["https://aebdf553-6d3e-4c14-9d7a-85891e1ff2db.predix.uaa.run.aws-usw02-dev.ice.predix.io/oauth/token"]}
-
-{"trustedIssuerIds": ["https://67c431e9-29e8-49e5-95b2-105660686261.predix-uaa.run.aws-usw02-pr.ice.predix.io/oauth/token"]}
-
 AppScalerImpl _as=new AppScalerImpl();
 this._accountProvisioner.getAccount(this._appName);
 
