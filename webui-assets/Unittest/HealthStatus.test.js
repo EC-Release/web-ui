3f65af79cb2e5afc7e969962873681f4266c269a
vidya22022 <76679290+vidya22022@users.noreply.github.com>
2021-03-25 18:15:00 +0530

    Create HealthStatus.test.js
    added new test file for Monitor tab ->helath status

---
+++ webui-assets/Unittest/HealthStatus.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Healthstatus from '../Monitor/Healthstatus.js';
+
+configure({adapter: new Adapter()});
+
+describe('Testing from HealthStatus.test.js <View />', () => {
+
+it('should render the View component', () => {
+  const wrapper = shallow(<Healthstatus />, { disableLifecycleMethods: true });
+  expect(wrapper).toBeTruthy();
+});
+    
+it('should have "Monitorhealthstatus" class with div only once', () => {
+  let demoAllFields = [
+      {gatewayId: "345435"}
+  ];
+  const wrapper = shallow(<Healthstatus createView={()=>{}} allFields={demoAllFields} selectedFields={[]} />);
+  expect(wrapper.find('div.Monitorhealthstatus').length).toBe(1);
+});
+});
