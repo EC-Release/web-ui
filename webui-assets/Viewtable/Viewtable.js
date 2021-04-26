59485c46f54c8b0dac9cb38357830746b560570b
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-04-26 18:19:03 +0530

    Update Viewtable.js

--- webui-assets/Viewtable/Viewtable.js
+++ webui-assets/Viewtable/Viewtable.js
@@ -16,7 +16,7 @@ export default class Viewtable extends React.Component {
         /* jshint ignore:start */
         /* istanbul ignore next */
         return(/* istanbul ignore next */
-            <table id="viewTable" className="table">
+          /*  <table id="viewTable" className="table">
                 <thead>
                     <tr>
                         {this.props.tableData.thead.map((val, index) => {
@@ -44,7 +44,66 @@ export default class Viewtable extends React.Component {
                         )
                     })}
                 </tbody>
-            </table>
+            </table>*/
+        <table className="table ">
+          <thead>
+            <tr>
+              {this.props.tableData.thead.map((val, index) => {
+                return <th key={"viewTableThead_" + index}>{val.fieldName}</th>;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.tableData.tbody.map((tbodyVal, tbodyIndex) => {
+              return (
+                <React.Fragment key={"viewTableTbodyTr_" + tbodyIndex}>
+                  <tr
+                    data-toggle="collapse"
+                    data-target={"#collapseme" + tbodyIndex}
+                  >
+                    {this.props.tableData.thead.map((val, tdIndex) => {
+                      return (
+                        <td key={"viewTableTbodyTd_" + tdIndex}>
+                          {}
+                          {tbodyVal[tdIndex].value} &nbsp;&nbsp;
+                          {tbodyVal[tdIndex].hiddenValue &&
+                          tbodyVal[tdIndex].hiddenValue != "" &&
+                          tbodyVal[tdIndex].hiddenState ? (
+                            <img
+                              onClick={this.props.showHideTableTdData.bind(
+                                this,
+                                tbodyIndex,
+                                tdIndex
+                              )}
+                              className="icon-arrowmore"
+                              alt="td-detail"
+                              src="assets/static/images/icon_arrowmore.svg"
+                            />
+                          ) : null}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                  <tr id={"collapseme" + tbodyIndex} className="collapse out">
+                    <td colSpan="8">
+                      <div>
+                        <Healthstatus
+                          userId={this.props.userId}
+                          showGlobalMessage={this.props.showGlobalMessage.bind(
+                            this
+                          )}
+                          hideGlobalMessage={this.props.hideGlobalMessage.bind(
+                            this
+                          )}
+                        />
+                      </div>
+                    </td>
+                  </tr>
+                </React.Fragment>
+              );
+            })}
+          </tbody>
+        </table>
         )
         /* jshint ignore:end */
     }
