75ff9bf787a90cb20f5f6f66d3524f77fb1ded52
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-05-05 20:52:39 +0530

    Update Maintainwatcherupgrade.js

--- webui-assets/Maintain/Maintainwatcherupgrade.js
+++ webui-assets/Maintain/Maintainwatcherupgrade.js
@@ -11,7 +11,7 @@ export default class Maintainwatcherupgrade extends React.Component {
     render() {
         /* jshint ignore:start */
         return (
-            <h1 className="text-center">Maintain Watcher Upgrade/Disable works!</h1>
+            <h1 className="text-center">Maintain Request Upgrade/Disable works!</h1>
         )
         /* jshint ignore:end */
     }
