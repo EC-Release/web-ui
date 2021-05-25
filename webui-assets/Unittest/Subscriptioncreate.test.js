c02b4acc4fd7caff81015d5a1d4427928e48b97b
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-05-25 17:39:24 +0530

    Update Subscriptioncreate.test.js

--- webui-assets/Unittest/Subscriptioncreate.test.js
+++ webui-assets/Unittest/Subscriptioncreate.test.js
@@ -39,31 +39,12 @@ describe('Testing from Subscriptioncreate.test.js <Subscriptioncreate />', () =>
         
         wrapper.setState({
             subscriptionForm:{
-                subscriptionName: { value: '', dirtyState: false },
-                subscriptionId: { value: '', dirtyState: false },
-                serviceUri: { value: '', dirtyState: false },
-                clientId: { value: '', dirtyState: false },
-                clientSecret: { value: '', dirtyState: false },
-                OAuth2: { value: '', dirtyState: false },
-                adminToken: { value: '', dirtyState: false },
-                applicationRole: { value: '', dirtyState: false },
-                bucAdn: { value: '', dirtyState: false },
-                compliance: { value: '', dirtyState: false },
-                confidentiality: { value: 'true', dirtyState: false },
-                customer: { value: '', dirtyState: false },
-                cluster: { value: '', dirtyState: false },
-                date: { value: '', dirtyState: false },
-                environment: { value: '', dirtyState: false },
-                managementHostType: { value: '', dirtyState: false },
-                optInoptOut: { value: '', dirtyState: false },
-                preserve: { value: '', dirtyState: false },
-                owner: { value: '', dirtyState: false },
-                project: { value: '', dirtyState: false },
-                security: { value: '', dirtyState: false },
-                version: { value: '', dirtyState: false },
-                app: { value: '', dirtyState: false },
-                assetId: { value: '', dirtyState: false },
-                uai: { value: '', dirtyState: false }
+                        licenseId: { value: "", dirtyState: false },
+                        emailAddress: { value: "", dirtyState: false },
+                        sso: { value: "", dirtyState: false },
+                        desc: { value: "", dirtyState: false },
+                        username: { value: "", dirtyState: false },
+                        date: { value: "", dirtyState: false }
             }
         });
         expect(wrapper).toBeTruthy();
@@ -73,23 +54,8 @@ describe('Testing from Subscriptioncreate.test.js <Subscriptioncreate />', () =>
         const propsToPass = {
             helpText: {
                 subscriptionName: '',
-                subscriptionId: '',
-                serviceUri: '',
-                clientId: '',
-                clientSecret: '',
-                OAuth2: '',
-                adminToken: '',
-                applicationRole: '',
-                bucAdn: '',
-                compliance: '',
-                customer: '',
-                cluster: '',
                 expdate: '',
-                managementHostType: '',
-                owner: '',
-                project: '',
-                security: '',
-                version: ''
+                username: ''
             }
         }
         fetchMock.get(`*`, JSON.stringify('SECONDGETOBJ'), { overwriteRoutes: false });
@@ -97,31 +63,12 @@ describe('Testing from Subscriptioncreate.test.js <Subscriptioncreate />', () =>
         
         wrapper.setState({
             subscriptionForm:{
-                subscriptionName: { value: '', dirtyState: false },
-                subscriptionId: { value: '', dirtyState: false },
-                serviceUri: { value: '', dirtyState: false },
-                clientId: { value: '', dirtyState: false },
-                clientSecret: { value: '', dirtyState: false },
-                OAuth2: { value: '', dirtyState: false },
-                adminToken: { value: '', dirtyState: false },
-                applicationRole: { value: '', dirtyState: false },
-                bucAdn: { value: '', dirtyState: false },
-                compliance: { value: '', dirtyState: false },
-                confidentiality: { value: 'true', dirtyState: false },
-                customer: { value: '', dirtyState: false },
-                cluster: { value: '', dirtyState: false },
-                date: { value: '', dirtyState: false },
-                environment: { value: '', dirtyState: false },
-                managementHostType: { value: '', dirtyState: false },
-                optInoptOut: { value: '', dirtyState: false },
-                preserve: { value: '', dirtyState: false },
-                owner: { value: '', dirtyState: false },
-                project: { value: '', dirtyState: false },
-                security: { value: '', dirtyState: false },
-                version: { value: '', dirtyState: false },
-                app: { value: '', dirtyState: false },
-                assetId: { value: '', dirtyState: false },
-                uai: { value: '', dirtyState: false }
+                        licenseId: { value: "", dirtyState: false },
+                        emailAddress: { value: "", dirtyState: false },
+                        sso: { value: "", dirtyState: false },
+                        desc: { value: "", dirtyState: false },
+                        username: { value: "", dirtyState: false },
+                        date: { value: "", dirtyState: false }
             }
         });
         expect(wrapper.find('div.subscription-form').length).toBe(1);
