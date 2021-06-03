269dd2af957481edb4458720e604edd8fad1e112
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-06-03 21:12:34 +0530

    Create Groupupgrade.test.js

---
+++ webui-assets/Unittest/Groupupgrade.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import fetchMock from 'fetch-mock';
+
+import Groupupgrade from '../Maintain/Groupupgrade';
+
+configure({ adapter: new Adapter() });
+
+window.initTable = () => { };
+window.destroyDataTable = () => { }; 
+
+describe('Testing from Groupupgrade.test.js <Groupupgrade />', () => {
+    it('should render the Groupupgrade component', () => {
+        fetchMock.get(`*`, JSON.stringify('SECONDGETOBJ'), { overwriteRoutes: false });
+        const wrapper = shallow(<Groupupgrade />, { disableLifecycleMethods: true });
+        expect(wrapper).toBeTruthy();
+    });
+});
