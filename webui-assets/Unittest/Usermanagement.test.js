7bc335b2655a1115f58a1f7bc46c84318ff938cc
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-06-03 20:49:28 +0530

    Update Usermanagement.test.js

--- webui-assets/Unittest/Usermanagement.test.js
+++ webui-assets/Unittest/Usermanagement.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Usermanagement from '../Settings/Usermanagement.js';
+import UserManagement from '../Settings/UserManagement.js';
 
 configure({adapter: new Adapter()});
 
-describe('Testing from Usermanagement.test.js <View />', () => {
+describe('Testing from UserManagement.test.js <View />', () => {
 
 it('should render the View component', () => {
-  const wrapper = shallow(<Usermanagement />, { disableLifecycleMethods: true });
+  const wrapper = shallow(<UserManagement />, { disableLifecycleMethods: true });
   expect(wrapper).toBeTruthy();
 });
     
