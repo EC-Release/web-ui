5dc392892e5fbefd6efda4083de75c37ae343ce4
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-06-04 13:36:43 +0530

    Update Maintainagentcreate.test.js

--- webui-assets/Unittest/Maintainagentcreate.test.js
+++ webui-assets/Unittest/Maintainagentcreate.test.js
@@ -11,64 +11,7 @@ window.enableToolTip = () => { };
 
 describe('Testing from Maintainagentcreate.test.js <Maintainagentcreate />', () => {
 
-    it('should render the Maintainagentcreate component', () => {
-        const propsToPass = {
-            helpText: {
-                mode: '',
-                environment: ''
-            }
-        }
-        fetchMock.get(`*`, JSON.stringify('SECONDGETOBJ'), { overwriteRoutes: false });
-        const wrapper = shallow(<Maintainagentcreate helpText={propsToPass.helpText} />, { disableLifecycleMethods: true });
-        
-        wrapper.setState({
-            agentForm: {
-                agentMode: { value: 1, dirtyState: false },
-                gateway: { value: '', dirtyState: false },
-                businessId: { value: 0, dirtyState: false },
-                businessName: { value: '', dirtyState: false },
-                requestor: { value: '', dirtyState: false },
-                requestedDate: { value: '', dirtyState: false },
-                customerEmail: { value: '', dirtyState: false },
-                bucAnd: { value: '', dirtyState: false },
-                vpc: { value: '', dirtyState: false },
-                debugMode: { value: true, dirtyState: false },
-                subscriptionId: { value: '', dirtyState: false },
-                ecVersion: { value: '', dirtyState: false },
-            }
-        });
-        expect(wrapper).toBeTruthy();
-    });
-
-    it('should have "GATEWAY" button with div only once', () => {
-        const propsToPass = {
-            helpText: {
-                mode: '',
-                environment: ''
-            }
-        }
-        fetchMock.get(`*`, JSON.stringify('SECONDGETOBJ'), { overwriteRoutes: false });
-        const wrapper = shallow(<Maintainagentcreate helpText={propsToPass.helpText} />, { disableLifecycleMethods: true });
-        
-        wrapper.setState({
-            agentForm: {
-                agentMode: { value: 1, dirtyState: false },
-                gateway: { value: '', dirtyState: false },
-                businessId: { value: 0, dirtyState: false },
-                businessName: { value: '', dirtyState: false },
-                requestor: { value: '', dirtyState: false },
-                requestedDate: { value: '', dirtyState: false },
-                customerEmail: { value: '', dirtyState: false },
-                bucAnd: { value: '', dirtyState: false },
-                vpc: { value: '', dirtyState: false },
-                debugMode: { value: true, dirtyState: false },
-                subscriptionId: { value: '', dirtyState: false },
-                ecVersion: { value: '', dirtyState: false },
-            }
-        });
-        expect(wrapper.find('div.gateway-form').length).toBe(1);
-    });
-
+    
     it('should have "SERVER" button with div only once', () => {
         const propsToPass = {
             helpText: {
