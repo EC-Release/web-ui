d9f9c55ebaa68d163a3a93e51fbfb81464d7cda6
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-06-22 18:29:28 +0530

    Update service-worker.js

--- webui-assets/static/js/service-worker.js
+++ webui-assets/static/js/service-worker.js
@@ -1,4 +1,5 @@
-//service workers for background sync and caching
+const cacheName = "v1";
+
 //service worker install
 self.addEventListener("install", (e) => {
   console.log("service Worker: Installed");
@@ -7,5 +8,33 @@ self.addEventListener("install", (e) => {
 //service worker activated
 self.addEventListener("activate", (e) => {
   console.log("service Worker: Activated");
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            console.log("service Worker: Clearing Old Cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
 
+//Call fetch event
+self.addEventListener("fetch", (e) => {
+  console.log("Service Worker : fetching");
+  e.resposndWith(
+    fetch(e.request)
+      .then((res) => {
+        //make copy/clone of response
+        const resClone = res.clone();
+        caches.open(cacheName).then((cache) => {
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch((err) => caches.match(e.request).then((res) => res))
+  );
+});
