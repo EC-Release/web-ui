0e24d8d1012b1b72910b90b7b7d1d8c5e259bb6e
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-06-22 16:50:48 +0530

    Update service-worker-comaptibilty.js

--- webui-assets/static/js/service-worker-comaptibilty.js
+++ webui-assets/static/js/service-worker-comaptibilty.js
@@ -8,20 +8,3 @@ if ("serviceWorker" in navigator) {
       .catch((err) => console.log(`Service Worker error : ${err}`));
   });
 }
-
- System.config({
-        transpiler: 'babel',
-        baseURL: '',
-        map: {
-          babel: 'https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.34/browser.min.js',
-          react: 'https://unpkg.com/react@17/umd/react.production.min.js',
-          'react-dom': 'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
-          uuid: "https://cdnjs.cloudflare.com/ajax/libs/uuid/8.3.2/uuid.min.js"
-        },
-        babelOptions: {
-          compact: true
-        }
-      });
-
-      // loads
-      System.import('assets/xcalrWebUI.js');
