2ca75997cca0cd1efbd72316dca6f08123e631f5
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-06-22 12:48:50 +0530

    Update custom.js

--- webui-assets/static/js/custom.js
+++ webui-assets/static/js/custom.js
@@ -1,13 +1,3 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("./service-worker.js")
-      .then(() => {
-        console.log("Service Worker Registered");
-      })
-      .catch((err) => console.log(`Service Worker error : ${err}`));
-  });
-}
 
 
 $( document ).ready(function() {
