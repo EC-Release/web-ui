d85a072e6431e1a405534690399d34a35e2b177f
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-06-25 13:16:06 +0530

    Update service-worker.js

--- webui-assets/service-worker.js
+++ webui-assets/service-worker.js
@@ -1,28 +1,30 @@
 const cacheName = "v1";
 
-/* const cacheAssets =[
+ const cacheAssets =[
   'index.html',
   'App.js',
-  'xcalrWebUI.js'
-] */
+  'xcalrWebUI.js',
+  'Dashboard/Dashboard.js ',
+  './FloaterHelp/FloaterHelp.js', 
+] 
 
 //service worker install
 self.addEventListener("install", (e) => {
   console.log("service Worker: Installed");
- /*   e.waitUntil(
+   e.waitUntil(
     caches
     .open(cacheName)
     .then(cache=>
     {  console.log('Service Worker: caching files');
       cache.addAll(cacheAssets)})
       .then(()=>self.skipWaiting())
-  ) */
+  ) 
 });
 
 //service worker activated
 self.addEventListener("activate", (e) => {
   console.log("service Worker: Activated");
-  /*   e.waitUntil(
+    e.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cache) => {
@@ -33,13 +35,13 @@ self.addEventListener("activate", (e) => {
         })
       );
     })
-  );  */
+  );  
 });
 
 //Call fetch event
 self.addEventListener("fetch", (e) => {
   console.log("Service Worker : fetching");
-   e.respondWith(
+/*   e.respondWith(
     fetch(e.request)
       .then((res) => {
         //make copy/clone of response
@@ -50,12 +52,12 @@ self.addEventListener("fetch", (e) => {
         return res;
       })
       .catch((err) => caches.match(e.request).then((res) => res))
-  ); 
+  ); */
  
- /*   e.respondWith(
+    e.respondWith(
     fetch(e.request)
     .catch(() => caches.match(e.request))
-  ) */
+  ) 
   
   
 });
