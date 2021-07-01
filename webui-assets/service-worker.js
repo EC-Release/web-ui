b2004d879039c65a6498b4d3405c6f3774a953c1
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-07-01 15:23:38 +0530

    Update service-worker.js

--- webui-assets/service-worker.js
+++ webui-assets/service-worker.js
@@ -1,3 +1,37 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// Note: Ignore the error that Glitch raises about workbox being undefined.
+workbox.setConfig({
+  debug: true,
+});
+
+workbox.precaching.precacheAndRoute([
+  "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.34/browser.min.js",
+  "https://unpkg.com/react@16/umd/react.development.js",
+  "https://unpkg.com/react-dom@16/umd/react-dom.development.js",
+  "https://unpkg.com/react-router-dom@5.0.0/umd/react-router-dom.min.js",
+]);
+
+// Demonstrates using default cache
+workbox.routing.registerRoute(
+    new RegExp('.*\\.(?:js)'),
+    new workbox.strategies.NetworkFirst(),
+);
+
+// Demonstrates a custom cache name for a route.
+workbox.routing.registerRoute(
+    new RegExp('.*\\.(?:png|jpg|jpeg|svg|gif)'),
+    new workbox.strategies.CacheFirst({
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          maxEntries: 3,
+        }),
+      ],
+    }),
+);
+
 const cacheName = "v1";
 
  const cacheAssets =[
