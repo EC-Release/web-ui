8e0a1b7e8afe2d7d7cf51e27ece4eac3dc543490
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-07-01 16:05:04 +0530

    Update custom.js

--- webui-assets/static/js/custom.js
+++ webui-assets/static/js/custom.js
@@ -4,6 +4,7 @@
         var message = { api: "/v1.2beta/ops/api/snapshot", authToken: authToken };
         myWorker.postMessage(message);
         myWorker.onmessage = function (e) {
+         sessionStorage.setItem("snapshotData", JSON.stringify(e.data.initialResult))
          sessionStorage.setItem("snapshotData", JSON.stringify(e.data.result))
         };
       }
