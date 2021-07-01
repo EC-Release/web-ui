8144166ced94a6e63b6aefcc8dcbf5b9ca96391d
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-07-01 16:06:47 +0530

    Update worker.js

--- webui-assets/worker.js
+++ webui-assets/worker.js
@@ -1,5 +1,22 @@
 
 this.onmessage = function (e) {
+  fetch(e.data.api, {
+      method: "GET",
+       headers: {
+       'Accept': 'application/json',
+       'Content-Type': 'application/json',
+       'Authorization':"Bearer " + e.data.authToken
+       }
+    }).then((response) => {
+      if (response.status === 200) {
+        response.json().then((respData) => {
+          this.postMessage({
+            initialResult: JSON.parse(JSON.stringify(respData)),
+          });
+        });
+      }
+    });
+  
   setInterval(() => {
     fetch(e.data.api, {
       method: "GET",
