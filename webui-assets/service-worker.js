23ad0e9f68c60d6fe8ea344fa6d2b879969f4360
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-07-14 19:34:00 +0530

    Update service-worker.js

--- webui-assets/service-worker.js
+++ webui-assets/service-worker.js
@@ -62,29 +62,6 @@ workbox.routing.registerRoute(
   })
 );
 
-self.addEventListener('install', event => {
-  console.log('V1 installing…');
-
-  // cache a cat SVG
-  event.waitUntil(
-    caches.open('static-v1').then(cache => cache.add('index.html'))
-  );
-});
-
-self.addEventListener('activate', event => {
-  console.log('V1 now ready to handle fetches!');
-});
-
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-
-  // serve the cat SVG from the cache if the request is
-  // same-origin and the path is '/dog.svg'
-  if (url.origin == location.origin) {
-    event.respondWith(caches.match(e.request));
-  }
-});
-
 /* const cacheName = "v1";
 
  const cacheAssets =[
