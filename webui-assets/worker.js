994fc60eac8ebcb7403e1f34f2de19203eed248d
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-07-19 18:58:28 +0530

    Update worker.js

--- webui-assets/worker.js
+++ webui-assets/worker.js
@@ -1,6 +1,7 @@
 
 this.onmessage = function (e) {
-   getSnapData(e)
+   getUserRole(e);
+   getSnapData(e);
   setInterval(() => {
     getSnapData(e)
   }, 300000);
@@ -25,5 +26,25 @@ function getSnapData(e){
     });
 }
 
+function getUserRole(e){
+ fetch(e.data.userApi, {
+      method: "GET",
+       headers: {
+       'Accept': 'application/json',
+       'Content-Type': 'application/json',
+       'Authorization':"Bearer " + e.data.authToken
+       }
+    }).then((response) => {
+      if (response.status === 200) {
+        response.json().then((respData) => {
+          console.log(respData)
+          this.postMessage({
+            user: respData,
+          });
+        });
+      }
+    });
+
+}
 
 
