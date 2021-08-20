d9c2fd99c393875fb558ff2fde773f53ce90ca6a
Subhash-vishwakarma <60516374+Subhash-vishwakarma@users.noreply.github.com>
2021-08-20 16:19:15 +0530

    Update App.js

--- webui-assets/App.js
+++ webui-assets/App.js
@@ -66,8 +66,10 @@ export default class App extends React.Component {
       notificationModal:{
         headerText:'',
         bodyText:'',
-        buttons:[]
+        buttons:[],
+        commands:[]
       },
+      isCommandPrompt:false,
       user: "OpsAdmin",
     };
   }
@@ -436,19 +438,25 @@ handleUser(user) {
       case 'copyToClipboard':
         this.copyToClipboard();
         break;
+      case 'closeModal':
+        window.hideNotificationModal();
+        break;  
     }
   }
 
   /* istanbul ignore next */
-  showModal(headerText,bodyText,buttons){
-    this.setState({
-      notificationModal:{
-        headerText :headerText,
-        bodyText:bodyText,
-        buttons:buttons
-      }
-    });
-    window.showNotificationModal();
+  showModal(headerText,bodyText,buttons,isCommandPrompt){
+      let cmndArr = bodyText.split('\n')
+        this.setState({
+        notificationModal:{
+            headerText :headerText,
+            bodyText:bodyText,
+            buttons:buttons,
+            commands:cmndArr
+        },
+        isCommandPrompt:isCommandPrompt
+        });
+      window.showNotificationModal();
   }
 
   render() {
@@ -515,12 +523,38 @@ handleUser(user) {
 
                     <div className="modal fade notificationModal" id="notificationModal" role="dialog" data-backdrop="static" data-keyboard="false">
                       <div className="modal-dialog modal-md">
-                        <div className="modal-content rounded-0">
+                        <div className={this.state.isCommandPrompt ? "modal-content rounded-0 command-prompt" : "modal-content rounded-0" }>
                           <div className="modal-header rounded-0">
                             <h6 className="modal-title text-middle">{this.state.notificationModal.headerText}</h6>
                           </div>
                           <div className="modal-body">
-                            <p> {this.state.notificationModal.bodyText} </p>
+                            {this.state.isCommandPrompt ?<div>
+                                    <pre>
+                                        <code>
+                                        {this.state.notificationModal.commands.map(
+                                            (cmd, indx) => {
+                                            return (
+                                                <p key={indx + "cmd"} className="text-left">
+                                                {cmd}
+                                                </p>
+                                            );
+                                            }
+                                        )}
+                                        </code>
+                                    </pre>
+                                    <button type="button" className="copy-icon" onClick={this.actionPerform.bind(this,"copyToClipboard")}>
+                                        <svg
+                                        viewBox="0 0 1000 1000"
+                                        height="25"
+                                        width="25"
+                                        role="img"
+                                        >
+                                        <title>Copy to Clipboard</title>
+                                        <path d="M704 896H64V320h640v192h64V192c0-35-29-64-64-64H512C512 57 455 0 384 0S256 57 256 128H64c-35 0-64 29-64 64v704c0 35 29 64 64 64h640c35 0 64-29 64-64V768h-64v128zM192 192h64s64-29 64-64 29-64 64-64 64 29 64 64 32 64 64 64h64s64 29 64 64H128c0-39 28-64 64-64zm-64 512h128v-64H128v64zm448-128V448L320 640l256 192V704h320V576H576zM128 832h192v-64H128v64zm320-448H128v64h320v-64zM256 512H128v64h128v-64z" />
+                                        </svg>
+                                    </button>
+                                </div> : <p> {this.state.notificationModal.bodyText} </p>
+                            }
                           </div>
                           <div className="modal-footer">
                             {this.state.notificationModal.buttons.map((button, buttonIndex) => {
